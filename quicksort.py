0bfa296f7262ce79d1130cb6f7015284087a7a61
Carlos Surillo <carlossurillo@gmail.com>
2025-08-21 15:51:08 -0400

    added and completed quicksort algorithm, as well as helper functions

---
+++ quicksort.py
@@ -0,0 +1,24 @@
+def quicksort(arr, low, high):
+    # Base case for recursion,
+    # when the subarray has 1 or 0 elements, or sorted
+    if low < high:
+        # Partition the array and get pivot
+        pi = partition(arr, low, high)
+
+        # Recursively sort left and right side of pivot
+        quicksort(arr, low, pi - 1)  # leftside
+        quicksort(arr, pi + 1, high)  # rightside
+
+
+def partition(arr, low, high):
+    pivot = arr[high]  # Choose the last element as pivot
+    i = low - 1  # Index of leftside of pivot boundary
+
+    # Loop through the array
+    for j in range(low, high):
+        if arr[j] < pivot:  # compare value with pivot
+            i += 1  # increment boundary position
+            arr[i], arr[j] = arr[j], arr[i]  # swap elements
+            # swap pivot with boundary
+            arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1  # Return the final position of the pivot
